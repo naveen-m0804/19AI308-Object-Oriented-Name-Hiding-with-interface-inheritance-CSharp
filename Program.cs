fd1b33369871198b06d5dc059cdca6f7af9e337b
Naveen M <117974950+naveen-m0804@users.noreply.github.com>
2024-05-09 09:25:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public interface IMario
+{
+    void Ability();
+}
+
+class Mario : IMario
+{
+    public virtual void Ability()
+    {
+        Console.WriteLine("Mario's normal ability is Jumping");
+    }
+}
+
+class SuperMario : Mario
+{
+    public new void Ability()
+    {
+        Console.WriteLine("Super Mario's ability is  Flying and shooting fireballs");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Mario mario = new Mario();
+        SuperMario superMario = new SuperMario();
+
+        mario.Ability(); 
+        superMario.Ability();
+    }
+}
+
